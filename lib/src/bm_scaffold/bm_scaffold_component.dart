32b955269dba3fb45694efad5ad8848bec6a6486
Samuel Odundo <samieodd@gmail.com>
2020-01-24 22:37:39 +0300

    Added model

--- lib/src/bm_scaffold/bm_scaffold_component.dart
+++ lib/src/bm_scaffold/bm_scaffold_component.dart
@@ -1,31 +1,40 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import '../model/bookmark.dart';
 
 @Component(
   selector: 'bm-scaffold',
   templateUrl: 'bm_scaffold_component.html',
-  directives: [coreDirectives],
+  directives: [coreDirectives, formDirectives],
 )
 class BookmarkScaffoldComponent {
   final List bookmarks = [
-    {
-      'title': 'Creative Bracket',
-      'description':
+    Bookmark(
+      title: 'Creative Bracket',
+      description:
           'Go-to Dart blog containing Dart resources for beginners and beyond',
-      'url': 'https://creativebracket.com',
-    },
-    {
-      'title': 'Dartlang Home',
-      'description': 'Documentation website for the Dart language and tools',
-      'url': 'https://dartlang.org',
-    },
-    {
-      'title': 'Flutter',
-      'description': 'Build native mobile apps with the Flutter SDK',
-      'url': 'https://flutter.io',
-    },
+      url: 'https://creativebracket.com',
+      edit: false,
+    ),
+    Bookmark(
+      title: 'Dartlang Home',
+      description: 'Documentation website for the Dart language and tools',
+      url: 'https://dartlang.org',
+      edit: false,
+    ),
+    Bookmark(
+      title: 'Flutter',
+      description: 'Build native mobile apps with the Flutter SDK',
+      url: 'https://flutter.io',
+      edit: false,
+    ),
   ];
 
   void removeBookmark(int index) {
     bookmarks.removeAt(index);
+  }
+
+  void editBookmark(int index) {
+    bookmarks[index].edit = true;
   }
 }
---
+++ lib/src/model/bookmark.dart
@@ -0,0 +1,11 @@
+class Bookmark {
+  Bookmark({
+    this.title,
+    this.description,
+    this.url,
+    this.edit,
+  });
+
+  String title, description, url;
+  bool edit;
+}
